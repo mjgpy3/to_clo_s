7c7ebd50c35ad04d1ad148abf03b2c6919c4ac32
Michael J. Gilliland <mjg.py3@gmail.com>
2014-08-16 11:27:27 -0400

    Added test for array with elements of different types

--- spec/lib/to_clo_s_spec.rb
+++ spec/lib/to_clo_s_spec.rb
@@ -3,7 +3,8 @@ require './lib/to_clo_s.rb'
 {
   Object => -> { Object.new },
   Symbol => -> { :some_symbol },
-  'An Empty Hash' => -> { {} }
+  'An Empty Hash' => -> { {} },
+  'An array with a bunch of weird things' => -> { [42, {}, :foobar, [], "Bears, Beets, Battlestar Galactica"] }
 }.each do |things_name, thing_builder|
 
   describe things_name do
